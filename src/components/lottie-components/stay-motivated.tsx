df4694d1421a14299c57f809a36a408c1fe9a2a3
NightUmbrella <githubumbrella@gmail.com>
2023-10-26 04:21:35 +0500

    update

--- src/components/lottie-components/stay-motivated.tsx
+++ src/components/lottie-components/stay-motivated.tsx
@@ -2,31 +2,32 @@ import React, { useEffect, useRef, useState } from "react";
 import LottieAnimation from "../lottieAnimation";
 //import fourAnimation from "../lottie-json/7.json"; // run the animation
 import fourAnimation from "../../lottie-json/7.json";
+import Lottie from "lottie-react";
 
 const StayMotivated = () => {
-  // const lottieRef = useRef(null);
+  const lottieRef = useRef(null);
   // const [animate, setAnimate] = useState(false);
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     if (lottieRef.current) {
-  //       const element = lottieRef.current;
-  //       const rect = element.getBoundingClientRect();
-  //       const viewportHeight = window.innerHeight;
-  //       console.log(viewportHeight)
+  useEffect(() => {
+    const handleScroll = () => {
+      if (lottieRef.current) {
+        const element = lottieRef.current;
+        const rect = element.getBoundingClientRect();
+        const viewportHeight = window.innerHeight;
+        console.log(viewportHeight);
 
-  //       if (rect.top < viewportHeight / 2 && rect.bottom > viewportHeight / 2) {
-  //         lottieRef.current.play();
-  //       } else {
-  //         lottieRef.current.pause();
-  //       }
-  //     }
-  //   };
+        if (rect.top < viewportHeight / 2 && rect.bottom > viewportHeight / 2) {
+          lottieRef.current.play();
+        } else {
+          lottieRef.current.pause();
+        }
+      }
+    };
 
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div className="flex items-center justify-center gap-x-28 mb-20">
       <div className="w-[500px] ">
@@ -39,7 +40,13 @@ const StayMotivated = () => {
           the owl.
         </p>
       </div>
-      <LottieAnimation animationData={fourAnimation} />
+      {/* <LottieAnimation animationData={fourAnimation} /> */}
+      <Lottie
+        animationData={fourAnimation}
+        lottieRef={lottieRef}
+        loop
+        className="w-[450px]"
+      ></Lottie>
     </div>
   );
 };
