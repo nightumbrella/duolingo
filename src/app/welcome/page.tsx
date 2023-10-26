d72af947776eb15f96a7edabf6369c1564959550
NightUmbrella <githubumbrella@gmail.com>
2023-10-27 00:59:39 +0500

    add welcome page ad the assets

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import LottieAnimation from "@/components/lottieAnimation";
+import React from "react";
+// ↓↓↓ for welcome route assets
+// import happyDuo from "../../../public/test/happyDuo.json";
+// import heyDuo from "../../../public/test/heyDuo.json";
+// import writeDuo from "../../../public/test/writeDuo.json";
+// import goodDuo from "../../../public/test/goodDuo.json";
+
+const Welcome = () => {
+  return (
+    <div>
+      <LottieAnimation />
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+
+export default Welcome;
