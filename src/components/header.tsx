4d8695c63882c68b6cd68d14a4a8e3588de12392
NightUmbrella <githubumbrella@gmail.com>
2023-10-26 16:38:11 +0500

    add scroll function in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
+import CustomButton from "./customButton";
+import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
+  const [scrollActive, setScrollActive] = useState<boolean>(false);
+  const headerRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const scrollDown = () => {
+      if (window.scrollY > 50) {
+        setScrollActive(true)
+      }else{
+        setScrollActive(false)
+      }
+    };
+    window.addEventListener("scroll", scrollDown);
+  }, []);
   return (
-    <div className="sticky top-0">
+    <div
+      ref={headerRef}
+      className={` ${scrollActive ? "border-b drop-shadow-sm" : ""} transition duration-500 fixed top-0  z-50 flex items-center  justify-between px-[400px] py-2 bg-white left-0 w-screen`}
+    >
       <Link href={"/"}>
-        <Image
-          src={"/public/logo.svg"}
-          fill
-          alt='logo'
-        />
+        <Image src={"/logo.svg"} fill alt="logo" className="!relative" />
       </Link>
-        <button>test</button>
+      <div>
+        <CustomButton className="uppercase transition w-[200px] bg-lime-500 font-semibold border-b-lime-700 border-b-[4px] hover:bg-lime-400">
+          get started
+        </CustomButton>
+      </div>
     </div>
   );
 };
