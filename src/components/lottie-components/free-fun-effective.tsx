042228b5786c4207150141440e0c37e93b9dd45f
NightUmbrella <githubumbrella@gmail.com>
2023-10-26 11:42:18 +0500

    add free fan effective components

--- src/components/lottie-components/free-fun-effective.tsx
+++ src/components/lottie-components/free-fun-effective.tsx
@@ -1,11 +1,41 @@
-import Lottie from "lottie-react";
-import React from "react";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import React, { useEffect, useRef } from "react";
 import secondAnimation from "../../lottie-json/10.json"; //free fan
 import Link from "next/link";
+import { handleScroll } from "./utils/handle-scroll";
 
 const FreeFanEffective = () => {
+  const lottieRef = useRef<LottieRefCurrentProps | null>(null);
+  const boxRef = useRef<HTMLDivElement | null>(null);
+  // const [animate, setAnimate] = useState(false);
+  useEffect(() => {
+    // const handleScroll = () => {
+    //   if (lottieRef.current && boxRef.current) {
+    //     const element = lottieRef.current;
+    //     // const boxRefAction = boxRef.current;
+    //     const rect: DOMRect = boxRef.current.getBoundingClientRect();
+    //     const viewportHeight = window.innerHeight;
+
+    //     if (rect.top < viewportHeight / 2 && rect.bottom > viewportHeight / 2) {
+    //       element.play();
+    //     } else {
+    //       element.pause();
+    //     }
+    //   }
+    // };
+
+    window.addEventListener("scroll", () => handleScroll(lottieRef, boxRef));
+    return () => {
+      window.removeEventListener("scroll", () =>
+        handleScroll(lottieRef, boxRef)
+      );
+    };
+  }, []);
   return (
-    <div className="flex items-center justify-center gap-x-28 mb-20">
+    <div
+      className="flex items-center justify-center gap-x-28 mb-20"
+      ref={boxRef}
+    >
       <div>
         <h1 className="text-[48px] text-lime-500 font-semibold mb-3">
           free. fun. effective.
@@ -21,7 +51,7 @@ const FreeFanEffective = () => {
       </div>
       <Lottie
         animationData={secondAnimation}
-        // lottieRef={lottieRef}
+        lottieRef={lottieRef}
         loop
         className="w-[450px]"
       ></Lottie>
