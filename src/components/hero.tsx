ea6109c98aff3f483a8a30446e584450aa831de1
NightUmbrella <githubumbrella@gmail.com>
2023-10-27 02:55:57 +0500

    update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,11 +3,39 @@ import CustomButton from "./customButton";
 import Link from "next/link";
 import LottieAnimation from "./lottieAnimation";
 import firstAnimation from "../lottie-json/4.json"; // hero animation phone
+import Lottie from "lottie-react";
 
 const Hero = () => {
+    const interactivity = {
+        mode: "",
+        actions: [
+          {
+            visibility: [0, 0.2],
+            type: "stop",
+            frames: [0],
+          },
+          {
+            visibility: [0.2, 0.45],
+            type: "seek",
+            frames: [0, 45],
+          },
+          {
+            visibility: [0.45, 1.0],
+            type: "loop",
+            frames: [45, 60],
+          },
+        ],
+      };
+      
   return (
     <div className="flex items-center justify-center h-[calc(100vh-80px)]">
-      <LottieAnimation animationData={firstAnimation} />
+      {/* <LottieAnimation animationData={firstAnimation} /> */}
+      <Lottie
+        animationData={firstAnimation}
+        loop
+        className="w-[450px]"
+      ></Lottie>
+      {/* <lottie-interactivity src></lottie-interactivity> */}
       <div
         className="
             flex
