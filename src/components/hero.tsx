270b208425015aff233c5959d59a736c3c2675f7
NightUmbrella <githubumbrella@gmail.com>
2023-10-27 10:20:03 +0500

    update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,41 +1,58 @@
+// "use client";
 import React from "react";
 import CustomButton from "./customButton";
 import Link from "next/link";
 import LottieAnimation from "./lottieAnimation";
+// import heroLottie from '../lottie-json/hero.lottie'
 import firstAnimation from "../lottie-json/4.json"; // hero animation phone
 import Lottie from "lottie-react";
+import { Controls, DotLottiePlayer } from "@dotlottie/react-player";
+import '@dotlottie/player-component';
+import "@dotlottie/react-player/dist/index.css";
 
 const Hero = () => {
-    const interactivity = {
-        mode: "",
-        actions: [
-          {
-            visibility: [0, 0.2],
-            type: "stop",
-            frames: [0],
-          },
-          {
-            visibility: [0.2, 0.45],
-            type: "seek",
-            frames: [0, 45],
-          },
-          {
-            visibility: [0.45, 1.0],
-            type: "loop",
-            frames: [45, 60],
-          },
-        ],
-      };
-      
+  //   const interactivity = {
+  //     mode: "cursor",
+  //     actions: [
+  //       {
+  //         visibility: [0, 0.2],
+  //         type: "stop",
+  //         frames: [0],
+  //       },
+  //       {
+  //         visibility: [0.2, 0.45],
+  //         type: "seek",
+  //         frames: [0, 45],
+  //       },
+  //       {
+  //         visibility: [0.45, 1.0],
+  //         type: "loop",
+  //         frames: [0, 60],
+  //       },
+  //     ],
+  //   };
+
   return (
     <div className="flex items-center justify-center h-[calc(100vh-80px)]">
       {/* <LottieAnimation animationData={firstAnimation} /> */}
-      <Lottie
+      {/* <Lottie
         animationData={firstAnimation}
         loop
         className="w-[450px]"
-      ></Lottie>
-      {/* <lottie-interactivity src></lottie-interactivity> */}
+      ></Lottie> */}
+      {/* <DotLottiePlayer
+        src={"https://static9.lottiefiles.com/lotties/01_ramen_character.lottie"}
+        autoplay
+        loop
+      ></DotLottiePlayer> */}
+       {/* <dotlottie-player
+        src="../lottie-json/hero.lottie.json"
+        autoplay
+        loop
+        style={{ height: '100%', width: '100%' }}
+      /> */}
+      
+      {/* <lottie-player src="" background="#ffffff"  speed="1"  style="width: 300px; height: 300px;" hover loop controls autoplay></lottie-player> */}
       <div
         className="
             flex
