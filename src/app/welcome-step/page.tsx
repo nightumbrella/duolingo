e12e2a08fcb394f557f5fe0ab202e194d1284fa8
NightUmbrella <githubumbrella@gmail.com>
2023-12-19 00:14:44 +0500

    add step image

--- src/app/welcome-step/page.tsx
+++ src/app/welcome-step/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import ProgressBar from "./_components/progress_bar";
 
 const WelcomeStepPage = () => {
+  const [progressStep, setProgressStep] = useState(1);
   return (
     <div>
-        
-    </div>
-  )
-}
+      <div>
+        <ProgressBar progress={progressStep} />
+      </div>
+      <div>
 
-export default WelcomeStepPage
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeStepPage;
+
+
+const firstStep = () => {
+    const firstStepData = [
+        {
+            id:1,
+            title:"TV",
+            icon:''
+        }
+    ]
+    return (
+        <div>
+            <h1>
+                How did you hear about Duolingo!
+            </h1>
+
+        </div>
+    )
+}
