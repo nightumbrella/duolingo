3b0e0c2c87779c6074cecf812b908240a9f9a232
NightUmbrella <githubumbrella@gmail.com>
2023-12-18 23:39:40 +0500

    temp finish welcome page

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import LottieAnimation from "@/components/lottieAnimation";
+import Image from "next/image";
 import React from "react";
-import a from "../../../public/check.svg";
 // ↓↓↓ for welcome route assets
 // import happyDuo from "../../../public/welcome/happyDuo.json";
 // import heyDuo from "../../../public/welcome/heyDuo.json";
@@ -10,10 +10,25 @@ import a from "../../../public/check.svg";
 
 const Welcome = () => {
   return (
-    <div>
-      <LottieAnimation />
-      <h1></h1>
-      {a}
+    <div className="flex items-center justify-center w-full">
+      <div className="relative mt-20">
+        {/* <LottieAnimation /> */}
+        <span className="border absolute bottom-32 left-12 rounded-xl px-3 py-1">
+          Hi there! I&apos;m NightDuo!
+        </span>
+        <Image
+          alt="sdf"
+          width={100}
+          height={100}
+          className="w-[300px] h-[300px]"
+          src={"/check.svg"}
+        />
+      </div>
+      <div className="border-t w-full absolute  bottom-20">
+        <button className="uppercase ml-auto text-white absolute -bottom-16 right-40  px-10 py-3 rounded-xl hover:bg-[#72d62b] transition font-semibold bg-[#4db302] border-b-[4px] border-[#c1ff60]">
+          continue
+        </button>
+      </div>
     </div>
   );
 };
